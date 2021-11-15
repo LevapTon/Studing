be8c7ed6182f60342e5df2e907686237eeb137f8
LevapTon <pp111011@mail.ru>
2021-11-15 14:13:55 +0500

    delete

--- AlgoPro/Task_1_Files.py
+++
@@ -1,9 +0,0 @@
-def sum_from_txt():
-    'Сумма чисел из блокнота'
-    with open('файлы\\Numbers_1.txt') as file:
-        res = 0
-        for line in file:
-            res += sum(map(int, line.split()))
-    print(res)
-
-sum_from_txt()
--- AlgoPro/a = ['a', 'b', 'c', 'd'].py
+++
@@ -1,9 +0,0 @@
-a = ['a', 'b', 'c', 'd']
-b = 'abcf'
-c = 'focr\n'
-for smb in c:
-    print(smb)
-    if smb != a:
-        print('+')
-    else:
-        print('-')
