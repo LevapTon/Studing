383b09e9eab6dc5b1ec28525fe4c42b30a6d7eb9
Pavel Plotnikov <90271486+LevapTon@users.noreply.github.com>
2022-12-19 21:03:13 +0500

    Update Car.cs

--- Разработка ПП/Абстрактные классы/Car.cs
+++ Разработка ПП/Абстрактные классы/Car.cs
@@ -4,71 +4,6 @@ namespace AbstractClasses
 {
     class Car: Vehicle
     {
-        string comp = "Без компании",  // Производитель
-            name = "Без названия";  // Наименование
-        int pas_num,  // Количество пассажиров
-            rel_year;  // Год выпуска
-        double cost;  // Стоимость
-
-        public override string Name
-        {
-            get
-            {
-                return name;
-            }
-            set
-            {
-                name = value; 
-            }
-        }
-        public override string Comp
-        {
-            get
-            {
-                return comp;
-            }
-            set
-            {
-                comp = value; 
-            }
-        }
-        public override int Pas_num
-        {
-            get
-            {
-                return pas_num;
-            }
-            set
-            {
-                if (value >= 0) pas_num = value;
-                else pas_num = 0;
-            }
-        }
-        public override int Rel_year
-        {
-            get
-            {
-                return rel_year;
-            }
-            set
-            {
-                if (value >= 1768) rel_year = value;  // 1768 – год создания первого автомобиля
-                else rel_year = 1768;
-            }
-        }
-        public override double Cost
-        {
-            get
-            {
-                return cost;
-            }
-            set
-            {
-                if (value >= 0) cost = value;
-                else cost = 0;
-            }
-        }
-
         public Car()
         {
             this.Comp = "Неизвестно";
@@ -77,18 +12,18 @@ namespace AbstractClasses
             this.Pas_num = 0;
             this.Cost = 0;
         }
-        public Car(string x1, string x2, int x3, int x4, double x5)
+        public Car(string comp, string name, int rel_year, int pas_num, double cost)
         {
-            this.Comp = x1;
-            this.Name = x2;
-            this.Rel_year = x3;
-            this.Pas_num = x4;
-            this.Cost = x5;
+            this.Comp = comp;
+            this.Name = name;
+            this.Rel_year = rel_year;
+            this.Pas_num = pas_num;
+            this.Cost = cost;
         }
 
         public override string PrintVehicle()
         {
-            return $"Автомобиль {Name}\nПроизводитель {Comp}, Кол-во мест {Pas_num}, Год выпуска {Rel_year} г., Стоимость {Cost} руб.";
+            return $"Автомобиль {Name}\nПроизводитель {Comp, -30}Кол-во мест {Pas_num, -5}Год выпуска {Rel_year, -6} г. Стоимость {Cost, -12} руб.";
         }
     }
 }
